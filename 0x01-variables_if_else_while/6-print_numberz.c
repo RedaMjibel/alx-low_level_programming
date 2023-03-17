7e2e23ee7e7c55ed62575fb173361eaac93346ae
redamjibel <redamjibel@hotmail.co>
2023-03-17 12:45:43 -0700

    numz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+/**
+ *  * main - Entry point
+ *   * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar('0' + n);
+}
+putchar('\n');
+return (0);
+}
