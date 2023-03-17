379750201e8749993e2eecfa0aebe473de32ef1b
redamjibel <redamjibel@hotmail.co>
2023-03-16 19:21:37 -0700

    change

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
+/**
+ *  main - A program that prints a string using puts function
+ *  return : 0 (success)
+ */
 int main(void)
 {
-	    
-	    char *c = "\"Programming is like building a multilingual puzzle";
-	    puts(c);
-
-	        return 0;
+    	puts("\"Programming is like building a multilingual puzzle");
+      	return (0);
 }
