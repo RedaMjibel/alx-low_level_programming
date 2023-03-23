aa2d336ffdc501b98b7d15fa491ce85e35daeabf
redamjibel <redamjibel@hotmail.co>
2023-03-22 18:08:01 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - calculates the sum of two given numbers
+ * @a: given number
+ * @b: second number to be added
+ * Return: the result value of addition
+ */
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
