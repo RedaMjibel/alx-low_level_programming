6e2afcecce3311142ea5b3819665e3c63ef05cf7
redamjibel <redamjibel@hotmail.co>
2023-03-23 20:01:02 -0700

    gfdgh

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 int i, j;
 for (j = 0; j <= 10; j++)
 {
-for (i = 0; y <= 14; y++)
+for (i = 0; i <= 14; i++)
 {
-if (y > 9)
+if (i > 9)
 {
-_putchar((y / 10) + '0');
+_putchar((i / 10) + '0');
 }
-_putchar((y % 10) + '0');
+_putchar((i % 10) + '0');
 }
 _putchar('\n');
 }
