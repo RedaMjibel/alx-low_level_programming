295109a3cc168531b9d7b0c1d5b7165456c53287
redamjibel <redamjibel@hotmail.co>
2023-03-27 11:27:48 -0700

    putss

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string followed by a new line
+ * @str: parameter
+ */
+void puts2(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str = str + 2;
+}
+_putchar('\n');
+}
