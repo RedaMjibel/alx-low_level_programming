1c19bc064ac27ae99c1711046104ef6cad946c04
redamjibel <redamjibel@hotmail.co>
2023-03-27 11:40:06 -0700

    putss

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,10 +6,15 @@
  */
 void puts2(char *str)
 {
-while (*str != '\0')
+	int i;
+	int j = 0;
+while (str[j] != '\0')
 {
-_putchar(*str);
-str = str + 2;
+j++;
+}
+for (i = 0; i < j; i += 2)
+{
+_putchar(str[i]);
 }
 _putchar('\n');
 }
