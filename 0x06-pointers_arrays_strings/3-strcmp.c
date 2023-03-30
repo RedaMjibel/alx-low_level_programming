7f7cdc191f3cb52ce423f4a2e92eedae5cefac40
redamjibel <redamjibel@hotmail.co>
2023-03-30 14:11:01 -0700

    wrong question

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,24 +4,15 @@
  * _strcmp - compare strings
  * @s2: string 2
  * @s1: string 1
- * Return: 0 or 1
+ * Return: difference of the first unmatched characters
  */
 
 int _strcmp(char *s1, char *s2)
 {
-int i = 0, f = 0;
-
-while (s1[i] != '\0' && s2[i] != '\0')
+while (*s1 && *s2 && *s1 == *s2)
 {
-if (s1[i] != s2[i])
-{
-f = 1;
-break;
+s1++;
+s2++;
 }
-i++;
-}
-if (f == 0)
-return (0);
-else
-return (1);
+return (*s1 - *s2);
 }
