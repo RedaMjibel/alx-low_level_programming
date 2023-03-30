619b3762e25e63f82ca77390b31aeedb88cc73bf
redamjibel <redamjibel@hotmail.co>
2023-03-30 13:39:37 -0700

    gfgh

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,17 +11,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0, j = 0;
-while (src[i] != '\0')
+while (src[i++])
 {
 j++;
 }
 for (i = 0; src[i] && i < n; i++)
-{
 dest[i] = src[i];
-}
+
 for (i = j; i < n; i++)
-{
 dest[i] = '\0';
-}
+
 return (dest);
 }
