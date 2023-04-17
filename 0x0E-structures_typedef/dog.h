01398577ef19fdf3b6b998e3722b036561d78eae
redamjibel <redamjibel@hotmail.co>
2023-04-17 15:06:54 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - Dog attributes
+ * @name: name of the dog
+ * @age: age of a dog
+ * @owner: the owner of the dog
+ *
+ * Description: The attributes of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
