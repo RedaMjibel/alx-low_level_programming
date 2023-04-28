65d58ffbb71c382ecc1e4f093e67235e192902c4
RedaMjibel <reda_mjibel@hotmail.com>
2023-04-27 17:15:29 -0700

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef _HEADER_
+#define _HEADER_
 #include <stddef.h>
 
 /**
@@ -8,11 +10,12 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-	    char *str;
-	        unsigned int len;
-		    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -20,3 +23,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
+#endif
