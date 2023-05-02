d6372c187316a2ab15f00e0eb71868cc941c3e94
RedaMjibel <reda_mjibel@hotmail.com>
2023-05-02 15:09:05 -0700

    bettymessingwithme

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - delete a node
+ * @head: double pointer to list
+ *
+ * Return: nothing
+ */
+
+int pop_listint(listint_t **head)
+{
+	struct listint_s *temp;
+	int data;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+		data = (*head)->n;
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+
+	return (data);
+}
