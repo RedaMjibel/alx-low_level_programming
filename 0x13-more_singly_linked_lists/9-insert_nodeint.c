17e80b61e8d5cfe7f5b6b8d3e7739decb0d4ed9c
RedaMjibel <reda_mjibel@hotmail.com>
2023-05-02 19:02:35 -0700

    please

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = ptr2;
 		return (ptr2);
 	}
-	while (i < idx)
+	while (temp && i < idx)
 	{
 		if (i == idx - 1)
 		{
