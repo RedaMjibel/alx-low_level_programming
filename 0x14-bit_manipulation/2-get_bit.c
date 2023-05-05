8e935be8e09dc7c0c75bad2538becdcc5f4cfc0c
RedaMjibel <reda_mjibel@hotmail.com>
2023-05-04 17:00:35 -0700

    shift

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: given integer
+ * @index: index
+ *
+ * Return: the converted number, or 0
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int val = 0;
+
+	if (index > 63)
+		return (-1);
+	val = (n >> index) & 1;
+
+	return (val);
+}
