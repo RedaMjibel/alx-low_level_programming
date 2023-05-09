adcd3024b479d06004d07be886275dcc9362e10b
RedaMjibel <reda_mjibel@hotmail.com>
2023-05-08 17:11:50 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 			i++;
 	}
 
-	fout = open(filename, O_CREAT | O_RDWR, 0600);
+	fout = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fout == -1)
 		return (-1);
 
