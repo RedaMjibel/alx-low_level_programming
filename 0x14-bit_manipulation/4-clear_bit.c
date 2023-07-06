41ec58f9f3f84f95504721248035d4cf78190de7
redamjibel <redamjibel@hotmail.com>
2023-07-06 13:57:46 -0700

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,11 +12,10 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int uint = 1;
 
 	if (index > 63)
 		return (-1);
-	*n = (~(uint << index) | *n);
+	*n = (~(1 << index) & *n);
 
 	return (1);
 }
