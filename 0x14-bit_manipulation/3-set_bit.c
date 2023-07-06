157e1fea57d8a9c3bb86ed46068b97ad0cd1a042
redamjibel <redamjibel@hotmail.com>
2023-07-06 13:39:59 -0700

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,11 +12,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int uint = 1;
 
 	if (index > 63)
 		return (-1);
-	*n = ((uint << index) | 1);
+	*n = ((1 << index) | *n);
 
 	return (1);
 }
