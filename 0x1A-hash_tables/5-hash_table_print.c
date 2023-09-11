2b5fdec86c89f3b9fadb109ca31b94f24dfbb4be
redamjibel <redamjibel@hotmail.com>
2023-09-10 18:48:43 -0700

    update

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -26,7 +26,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (check == 1)
 				printf(", ");
-			printf("%s: %s", node->key, node->value);
+			printf("'%s': '%s'", node->key, node->value);
 			check = 1;
 			node = node->next;
 		}
