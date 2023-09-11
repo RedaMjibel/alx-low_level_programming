f081119b0670a65930c3ed08a39e888c84b4921b
redamjibel <redamjibel@hotmail.com>
2023-09-10 18:08:07 -0700

    update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,7 +8,7 @@
  * @ht: a pointer to the hash table
  * @key: the key
  *
- * Return; the value associated with the element, or NULL
+ * Return: the value associated with the element, or NULL
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
@@ -22,7 +22,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	index = hash_djb2((const unsigned char *)key) % ht->size;
 	current = ht->array[index];
-	
+
 	while (current)
 	{
 		if (strcmp(current->key, key) == 0)
