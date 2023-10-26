01fb6af3e4acef8e7a3745c99d49a9ad3690b8c1
redamjibel <redamjibel@hotmail.com>
2023-10-25 19:45:52 -0700

    pycodestyle and indentation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Documentation """
 
+
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
     perimeter = 0
@@ -10,9 +11,12 @@ def island_perimeter(grid):
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
-                next_cell = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
+                next_cell = [(row - 1, col), (row + 1, col),
+                             (row, col - 1), (row, col + 1)]
 
                 for c_row, c_col in next_cell:
-                    if c_row < 0 or c_row >= rows or c_col < 0 or c_col >= cols or grid[c_row][c_col] == 0:
+                    if c_row < 0 or c_row >= rows or c_col < 0:
+                        perimeter = perimeter + 1
+                    if c_col >= cols or grid[c_row][c_col] == 0:
                         perimeter = perimeter + 1
     return perimeter
