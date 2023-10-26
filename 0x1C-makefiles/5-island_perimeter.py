ec2a2f6c8e05ed2bf0acba3182d56f9400f61757
redamjibel <redamjibel@hotmail.com>
2023-10-25 19:41:47 -0700

    trying to find the adjacent cell

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,7 +10,7 @@ def island_perimeter(grid):
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
-                next_cell = [(row - 1, col), (row + 1, col), (row, col + 1)]
+                next_cell = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
 
                 for c_row, c_col in next_cell:
                     if c_row < 0 or c_row >= rows or c_col < 0 or c_col >= cols or grid[c_row][c_col] == 0:
